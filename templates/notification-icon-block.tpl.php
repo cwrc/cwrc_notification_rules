357e48fbeddc4abb88e1dcdcb1a09d95dd666e18
Luke Bainbridge <luke@echidna.ca>
2016-02-19 13:30:28 -0500

    Adding notification icon block

---
+++ templates/notification-icon-block.tpl.php
@@ -0,0 +1,6 @@
+<div class="notification-icon-wrapper">
+  <a href="<?php print url('messages'); ?>">
+    <span class="badge notification-count"><?php print $unread_count; ?></span>
+    <span class="notification-icon"><?php print $unread_message; ?></span>
+  </a>
+</div>
