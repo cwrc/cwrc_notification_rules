52beb2dd846a821acc69755672343a5181c26ca1
Jacob Parry <jparry@echidna.ca>
2016-04-04 09:33:15 -0400

    Add class to <li> for styling

--- templates/notification-icon-block.tpl.php
+++ templates/notification-icon-block.tpl.php
@@ -1,4 +1,4 @@
-<li class="navbar-right">
+<li class="navbar-right nav-notifications">
   <a href="<?php print url('messages'); ?>">
     <span class="badge notification-count"><?php print $unread_count; ?></span>
     <span class="notification-icon"><?php print $unread_message; ?></span>
