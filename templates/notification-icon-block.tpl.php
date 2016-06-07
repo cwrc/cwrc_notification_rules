8a59c0382b0d9e3e491ebca9672127e4fb33f0e7
Jacob Parry <jparry@echidna.ca>
2016-06-07 10:05:10 -0400

    Hide notifications badge when there are no notifications

--- templates/notification-icon-block.tpl.php
+++ templates/notification-icon-block.tpl.php
@@ -1,6 +1,6 @@
 <li class="navbar-right nav-notifications">
   <a href="<?php print url('messages'); ?>">
-    <span class="badge notification-count"><?php print $unread_count; ?></span>
+    <span class="badge notification-count<?php if ($unread_count <= 0) echo " notification-count-none"; ?>"><?php print $unread_count; ?></span>
     <span class="notification-icon"><?php print $unread_message; ?></span>
   </a>
 </li>
